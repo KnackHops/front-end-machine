5b220bc1bef68a3753d46c0708a50730e0d0d7ed
Aldrian <himalosc@gmail.com>
2021-10-28 22:31:09 +0800

    added the corresponding prices

--- src/Components/Home/Sections/MenuFood.js
+++ src/Components/Home/Sections/MenuFood.js
@@ -5,45 +5,85 @@ const MenuFood = ( { menuClassChange } ) => {
     const pancitList = [
         {
             label: "Batil Patong",
-            price: "60"
+            priceList: [
+                {
+                    label: "Regular",
+                    price: "60"
+                },
+                {
+                    label: "Special",
+                    price: "80"
+                },
+                {
+                    label: "Jumbo",
+                    price: "100"
+                },
+                {
+                    label: "Overload",
+                    price: "150"
+                },
+            ]
         },
         {
             label: "Lomi",
-            price: "60"
+            priceList: [
+                {
+                    label: "Regular",
+                    price: "60"
+                },
+                {
+                    label: "Overload",
+                    price: "120"
+                },
+            ]
+        },
+        {
+            label: "Pancit Concha",
+            priceList: [
+                {
+                    label: "Regular",
+                    price: "60"
+                },
+                {
+                    label: "Special",
+                    price: "80"
+                },
+            ]
         },
         {
             label: "Mami",
             price: "60"
         },
         {
-            label: "Pancit Concha",
-            price: "60"
-        },
-        {
             label: "Pancit Sisig",
-            price: "60"
+            price: "100"
         }
     ]
 
     const bilaoList = [
         {
             label: "Bilao Batil Patong",
-            price: "60"
-        },
-        {
-            label: "Bilao Concha",
-            price: "60"
-        },
-        {
-            label: "Bilao Pancit Sisig",
-            price: "60"
+            priceList: [
+                {
+                    label: "Small",
+                    price: "330"
+                },
+                {
+                    label: "Medium",
+                    price: "530"
+                },
+                {
+                    label: "Large",
+                    price: "1030"
+                },
+            ]
         }
     ]
 
     const sideFoodList = [
         {
             label: "Lumpia",
-            price: "60"
+            price: "5"
         },
         {
             label: "Buko Halo-Halo",
--- src/Components/Home/Sections/MenuFoodInside.js
+++ src/Components/Home/Sections/MenuFoodInside.js
@@ -1,23 +1,50 @@
 const MenuFoodInside = ({classCon, headerLabel, itemList}) => {
     return (
         <div className={classCon}>
-                        <h3>{headerLabel}</h3>
-                        <ul>
-                            {   itemList.map( ( item, i ) => {
-                                    return (
-                                        <li className="fd" key={item.label + i}>
-                                            <span>
-                                                {item.label}
-                                            </span>
-                                            <span>
-                                                {item.price}
-                                            </span>
-                                        </li>
-                                    )
-                                })
+            <h3>{headerLabel}</h3>
+             <ul>
+            {   itemList.map( ( item, i ) => {
+                return (
+                        <li className="parent_list" key={item.label + i}>
+                            {
+                                item?.price ? 
+                                <p className="fd">
+                                    <span>
+                                        { item.label }
+                                    </span>
+                                    <span> { item.price } </span>
+                                </p>
+                                 :
+                                 <>
+                                    <p>
+                                        { item.label }
+                                    </p>
+                                    <ul>
+                                        {
+                                            item.priceList.map ( ( item, i ) => {
+                                                return (
+                                                    <li className="child_list">
+                                                        <p className="fd">
+                                                            <span>
+                                                                { item.label }
+                                                            </span>
+                                                            <span>
+                                                                { item.price }
+                                                            </span>
+                                                        </p>
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                 </>
                             }
-                        </ul>    
-                    </div> 
+                        </li>
+                )
+                })
+            }
+            </ul>    
+        </div> 
     )
 }
 
